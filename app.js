ce37eb70c4b063a678f6859c24764c9f2f4fb48d
Matthew Baugh <acieffe@gmail.com>
2021-04-24 17:57:36 -0400

    Started wdwshiftx anew

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mysql = require('mysql');
+
+// Creating Database Connection
+// const db = mysql.createConnection({
+// 	host: 'localhost',
+// 	user: 'root',
+// 	password: '123456789',
+// 	database: 'wdwshiftx-test',
+// });
+
+// // Connecting to Database
+// db.connect((err) => {
+// 	if (err) {
+// 		throw err;
+// 	}
+// 	console.log('MySQL Connected...');
+// });
+
+const app = express();
+
+app.get('/', (req, res) => {
+	res.send('Hello from the other side!');
+});
+
+app.listen('3000', () => {
+	console.log('Server Started on port 3000');
+});
