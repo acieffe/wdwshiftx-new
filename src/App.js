e75ba6d1acec31ff2f13ea4ef8e064caf44aa53e
Matthew Baugh <acieffe@gmail.com>
2021-05-10 12:55:29 -0400

    something

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import { response } from 'express';
 import './App.css';
 
 function App() {
-	return <div className="App">Hello!</div>;
+
+	state = {
+		shifts: []
+	}
+
+	componentDidMount() {
+		this.getShifts();
+	}
+
+	getShifts = _ => {
+		fetch('http://localhost:3000/shifts')
+		.then(response => response.json())
+		.then(response => this.setState({ shifts: response.data}))
+		.catch(err => console.log(err))
+	}
+
+	renderShift = ({ id, shiftName }) => <div key={id}>{shiftName}</div>
+
+	return <div className="App">Hello!
+		{shifts.map(this.renderShift)}
+	</div>;
 }
 
 export default App;
